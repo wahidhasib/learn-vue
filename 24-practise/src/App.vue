8a31f51a71e9c6dced5e7551628b6ab06a4aa45a
Wahid Hasib <wahidhasib6@gmail.com>
2026-01-13 16:49:26 +0600

    Practise to use emit & events and make a small product project

---
+++ 24-practise/src/App.vue
@@ -0,0 +1,56 @@
+<script setup>
+import { reactive, ref } from 'vue';
+import Products from './components/Products.vue';
+
+  const authorName = ref("Bd Stall");
+  const products = reactive([
+    {
+      name:"Headphone",
+      category: "Electronics",
+      quantity: 500,
+      isPublished: true,
+    },
+    {
+      name:"Phone Case",
+      category: "Phone Accessories",
+      quantity: 100,
+      isPublished: true,
+    },
+    {
+      name:"T-shirt",
+      category: "Fashion",
+      quantity: 250,
+      isPublished: false,
+    },
+    {
+      name:"Poem Books",
+      category: "Books",
+      quantity: 200,
+      isPublished: false,
+    },
+  ]);
+</script>
+
+<template>
+  <div class="contaner mt-5">
+    <div class="row mx-3">
+      <div class="col-12">
+        <div class="input-group">
+          <span class=" input-group-text">Test</span>
+          <input type="text" v-model="authorName" placeholder="Enter text to check" class="form-control">
+        </div>
+      </div>
+
+      <div class="col-12 mt-2" v-for="product in products" :key="product.id">
+        <Products 
+        :name="product.name"
+        :category="product.category"
+        :isPublished="product.isPublished"
+        :quantity="product.quantity"
+        :authorName="authorName"
+        @active-update="product.isPublished = !product.isPublished"
+        ></Products>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ 24-practise/src/components/Products.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="card">
+        <div class="card-header d-flex align-items-center justify-content-between">
+            <h5 class="card-title">{{ name }}</h5>
+            <div class="form-group">
+                <label class=" form-check-label me-1">Active</label>
+                <input type="checkbox" @change="activeToggler" :checked="isPublished" class="form-check-input">
+            </div>
+        </div>
+        <div class="card-body mb-0 alert" :class="{'alert-success': isPublished, 'alert-danger': !isPublished}">
+            <h4>Category: <span class="fw-bold">{{ category }}</span></h4>
+            <p class="mb-1">Quantity: <span class="fw-bold">{{ quantity }}</span></p>
+            <p class="mb-1">Status: <span class="fw-bold">{{ isPublished }}</span></p>
+        </div>
+
+        <div class="card-footer">
+            <p class="mb-1">Authorized by author {{ authorName }}.</p>
+        </div>
+    </div>
+</template>
+
+<script setup>
+    const emit = defineEmits(["active-update"]);
+
+    function activeToggler() {
+        emit('active-update');
+    }
+    
+    const props = defineProps(
+        {
+            name: String,
+            category: String,
+            isPublished: Boolean,
+            quantity: Number,
+            authorName: String,
+        }
+    )
+</script>
