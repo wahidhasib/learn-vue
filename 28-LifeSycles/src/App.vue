af34d8ed57a5ccd94876e474983a5015b45a1bc2
Wahid Hasib <wahidhasib6@gmail.com>
2026-01-17 22:01:37 +0600

    Learn about slot & scope styling in vue

---
+++ 28-LifeSycles/src/App.vue
@@ -0,0 +1,48 @@
+<script setup>
+import { onBeforeMount, onBeforeUnmount, onBeforeUpdate, onMounted, onUnmounted, onUpdated, ref } from 'vue';
+import Counter from './Counter.vue';
+
+onBeforeMount(() => {
+  console.log('onBeforeMount called from App.vue');
+});
+
+onMounted(() => {
+  console.log('onMounted called from App.vue');
+});
+
+onBeforeUpdate(() => {
+  console.log('onBeforeUpdate called from App.vue');
+});
+
+onUpdated(() => {
+  console.log('onUpdated called from App.vue');
+});
+
+onBeforeUnmount(() => {
+  console.log('onBeforeUnmount called from App.vue');
+});
+
+onUnmounted(() => {
+  console.log('onUnmounted called from App.vue');
+});
+
+const count = ref(0);
+</script>
+
+<template>
+  <div>
+    <h1>Counter count: {{ count }}</h1>
+    <button @click="count++">Increase</button>
+    <button @click="count--">Decrease</button>
+    <Counter v-if="count > 0">
+      <h4>Parent slot from App.vue</h4>
+      <template v-slot:childSlot>
+        <p>Count is positive!</p>
+      </template>
+    </Counter>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ 28-LifeSycles/src/Counter.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <slot name="childSlot"></slot>
+        <h1>Counter from Counter.vue</h1>
+        <button @click="count++">Count: {{ count }}</button>
+
+        <slot></slot>
+
+    </div>
+</template>
+
+<script setup>
+import { onBeforeMount, onBeforeUnmount, onBeforeUpdate, onMounted, onUnmounted, onUpdated, ref } from 'vue';
+
+onBeforeMount(() => {
+    console.log('onBeforeMount called from Counter.vue');
+});
+
+onMounted(() => {
+    console.log('onMounted called  from Counter.vue');
+});
+
+onBeforeUpdate(() => {
+    console.log('onBeforeUpdate called  from Counter.vue');
+});
+
+onUpdated(() => {
+    console.log('onUpdated called  from Counter.vue');
+});
+
+onBeforeUnmount(() => {
+    console.log('onBeforeUnmount called  from Counter.vue');
+});
+
+onUnmounted(() => {
+    console.log('onUnmounted called  from Counter.vue');
+});
+
+const count = ref(0);
+</script>
+
+<style scoped>
+h1 {
+    color: teal;
+}
+</style>
